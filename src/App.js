07c28d6f7d83105b0a22d5eff4cc706993faa733
Fuwn <17520134@gm.uit.edu.vn>
2020-04-25 19:43:32 +0700

    minor changes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
 			.then((users) => this.setState({robots: users}));
-	}
+	} 
 
 	onSearchChange = (event) => {
 		this.setState({inputField: event.target.value});
