d911a9d28c8ac7fd61a2e18d4f287dc2e25ba8b0
Fuwn <17520134@gm.uit.edu.vn>
2020-04-25 19:58:23 +0700

    Updates

---
+++ precache-manifest.81d2b2959254b56f3bd23833b02045f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3398d145f6ae0d174da3cf6604aaf962",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "c0cf888ac0cbc0d4ee07",
+    "url": "/robofriends/static/css/main.fa1c27f9.chunk.css"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/robofriends/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0cf888ac0cbc0d4ee07",
+    "url": "/robofriends/static/js/main.5eba21a8.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
